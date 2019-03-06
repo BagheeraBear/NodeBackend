a48e7981bafd028a86e7eeb5be6597a51d3437d1
Ulf Swedin <ulf@swedin.nu>
2019-03-06 15:58:08 +0100

    Added yarn

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const chalk = require("chalk");
 const getNotes = require("./notes.js");
+const yargs = require("yargs");
+
+yargs.version("1.1.0");
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new note!");
+  }
+});
+
+console.log(yargs.argv);
 
 const command = process.argv[2];
 
