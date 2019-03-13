26ffda53e4d2da1bd982cf3f729336ae012249ca
Ulf Swedin <ulfswedin@Ulfs-MacBook-Air.local>
2019-03-13 10:39:56 +0100

    Chain Callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,6 +6,9 @@ const geocode = require("./utils/geocode.js");
 const forecast = require("./utils/forecast.js");
 
 geocode("Stockholm", (error, data)=>{
+  if(error){
+    return console.log(error)
+  }
   console.log("Error: ", error);
   console.log("Data: ", data);
   
