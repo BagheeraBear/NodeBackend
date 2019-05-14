68bb895095d16ff5b3ce864798bccec620242f2b
Ulf Swedin <ulf.swedin@hantverksdata.se>
2019-05-14 13:25:11 +0200

    Stash

---
+++ playground/destruction.js
@@ -0,0 +1,11 @@
+const product = {
+  label: "Red Notebook",
+  price: 3,
+  stock: 201,
+  salePrice: 185
+};
+
+// Object Destructuring
+const { label, stock } = product;
+console.log(label);
+console.log(stock);
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,23 @@
 // npm install request@2.88.0
 // Hemdashboard
+// const yargs = require("yargs");
+
+const argv = process.argv[2];
 
 const request = require("request");
 const geocode = require("./utils/geocode.js");
 const forecast = require("./utils/forecast.js");
 
-geocode("Stockholm", (error, data)=>{
-  if(error){
-    return console.log(error)
+geocode(argv, (error, data) => {
+  if (error) {
+    return console.log(error);
   }
-  console.log("Error: ", error);
-  console.log("Data: ", data);
-  
-  forecast(data.latitude, data.longitude, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
 
-})
-
-
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(data.location);
+    console.log(forecastData);
+  });
+});
