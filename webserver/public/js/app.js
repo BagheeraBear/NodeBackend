842558faf8d75d319041aca8f2a8dd3f8824f4ad
Ulf Swedin <ulf.swedin@hantverksdata.se>
2019-05-15 15:46:52 +0200

    Client side fetch

--- webserver/public/js/app.js
+++ webserver/public/js/app.js
@@ -1,1 +1,33 @@
-console.log('Client side javascript loaded!');
+console.log("Client side javascript loaded!");
+
+// fetch("http://puzzle.mead.io/puzzle").then(response => {
+//   response.json().then(data => {
+//     console.log(data);
+//   });
+// });
+
+const weatherForm = document.querySelector("form");
+
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const location = search.value;
+
+  if (location.length < 1) {
+    return console.log("You need to submit a location");
+  }
+  // fetch with weatherdata
+  fetch("http://localhost:3000/weather?address=" + location).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });
+
+  //console.log(location);
+});
